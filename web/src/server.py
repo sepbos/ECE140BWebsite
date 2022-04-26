4586f86a17026366676d6aad6c3fca5df82a962b
sepbos <sbostanb@ucsd.edu>
2022-04-25 23:22:42 -0700

    major addajgio

--- web/src/server.py
+++ web/src/server.py
@@ -1,37 +1,41 @@
-from wsgiref.simple_server import make_server
 from pyramid.config import Configurator
 from pyramid.renderers import render_to_response
-
+from pyramid.response import FileResponse
 import mysql.connector as mysql
 import os
+from wsgiref.simple_server import make_server
+
 
 db_user = os.environ['MYSQL_USER']
 db_pass = os.environ['MYSQL_PASSWORD']
 db_name = os.environ['MYSQL_DATABASE']
 db_host = os.environ['MYSQL_HOST']
 
-def get_home(req):
-  # Connect to the database and retrieve the users
-  db = mysql.connect(host=db_host, database=db_name, user=db_user, passwd=db_pass)
-  cursor = db.cursor()
-  cursor.execute("select first_name, last_name, email from Users;")
-  records = cursor.fetchall()
-  db.close()
 
-  return render_to_response('templates/home.html', {'users': records}, request=req)
+def kvp_page(req):
+    return FileResponse("templates/kvp.html")
 
-''' Route Configurations '''
+
+def product_page(req):
+    return FileResponse("templates/product.html")
+
+
+def home_page(req):
+    return FileResponse("templates/home.html")
+
+
 if __name__ == '__main__':
-  config = Configurator()
+    with Configurator() as config:
+        config.add_route('home', '/')
+        config.add_view(home_page, route_name='home')
 
-  config.include('pyramid_jinja2')
-  config.add_jinja2_renderer('.html')
+        config.add_route('kvp', '/kvp')
+        config.add_view(kvp_page, route_name='kvp', renderer='json')
 
-  config.add_route('get_home', '/')
-  config.add_view(get_home, route_name='get_home')
+        config.add_route('product', '/product')
+        config.add_view(product_page, route_name='product', renderer='json')
 
-  config.add_static_view(name='/', path='./public', cache_max_age=3600)
-
-  app = config.make_wsgi_app()
-  server = make_server('0.0.0.0', 6000, app)
-  server.serve_forever()
+        config.add_static_view(name='/', path='./public', cache_max_age=3600)
+        app = config.make_wsgi_app()
+    server = make_server('0.0.0.0', 6000, app)
+    server.serve_forever()
