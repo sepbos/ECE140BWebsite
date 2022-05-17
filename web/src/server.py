1704e595978fd79adda30988bf47b006f450ff7b
sepbos <sbostanb@ucsd.edu>
2022-05-17 16:10:15 -0700

    syntax

--- web/src/server.py
+++ web/src/server.py
@@ -29,7 +29,7 @@ def benefits_page(req):
 
 
 def interaction_page(req):
-    return FileResponse("templates/benefits.html")
+    return FileResponse("templates/interaction.html")
 
 
 def home_page(req):
